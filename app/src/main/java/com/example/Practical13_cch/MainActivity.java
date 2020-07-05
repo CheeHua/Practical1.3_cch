b41a5a69a5a2ba08bb5af896d23a08c1bdda7bc2
CheeHua <chiach-wm19@student.tarc.edu.my>
2020-07-05 17:43:29 +0800

    Commited by CheeHua in 5/7/2020

---
+++ app/src/main/java/com/example/Practical13_cch/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.Practical13_cch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
